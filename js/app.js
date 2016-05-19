b8ed8a4f36539bad6c9f7c582e0f8346da562912
Ovidio R. Calvet <calveto@gmail.com>
2016-05-19 00:20:37 -0400

    Added my first animation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
-$(function() {
-  alert('jquery ready');
-});
+//$(document).on('DOMContentLoaded', function() {
+//  var box = $('#box');
+//  console.log('box ready', box);
+//  TweenLite.to(box, 3, { x: 600 });
+//  TweenLite.to(box, 2, {top:300 });
+//  TweenLite.to(box, 2, { opacity:0 });
+//});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var box = document.getElementById('box');
+  TweenLite.to(box, 3, { x: 600 });
+  console.log('animating maybe', box);
+})
